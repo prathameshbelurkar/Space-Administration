b604e099bef570d098b3696ebe092a9ae213a213
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-21 23:11:51 +0530

    cors fixed

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const planetsRouter = require("./routes/planets/planets.router");
 
 // Express Instance
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(planetsRouter);
 
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
+
 // Importing Controllers
 const { getAllPlanets } = require("./planets.controller");
 
 const planetsRouter = express.Router();
 
-//
+// Setting Paths
 planetsRouter.get("/planets", getAllPlanets);
 
 module.exports = planetsRouter;
