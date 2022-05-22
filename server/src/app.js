1c9692ea45ead54dccf2e8dcf323d3c9163b5abd
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-22 12:08:30 +0530

    morgan: tracking user requests

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,28 @@
 const path = require("path");
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
 // Express Instance
 const app = express();
 
-// Middleware
+// Middlewares
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
