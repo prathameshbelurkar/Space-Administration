9ed7647ff07d757eb9e399ba922a646f9ac53bbb
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-05-22 10:04:27 +0530

    server: add planets list

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require("../../models/planets.model");
+const { planets } = require("../../models/planets.model");
 
 // Handling Responses and Requests
 function getAllPlanets(req, res) {
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,22 @@
 const http = require("http");
 const app = require("./app");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 // Creating Server
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-// Listening to Server
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+// Start Server
+async function startServer() {
+  // Load Planets Data
+  await loadPlanetsData();
+
+  // Listening to Server
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+// Call Start Server
+startServer();
